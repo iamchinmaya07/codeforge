cd021c661942287c66d61df435403618223141b2
user.email <cs172668@gmail.com>
2026-04-19 16:41:06 +0530

    update backend url

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,10 +1,8 @@
 import axios from "axios"
 
 const axiosClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL: 'https://codeforge-backend-iys9.onrender.com',
+  withCredentials: true,
 });
+
 export default axiosClient;
