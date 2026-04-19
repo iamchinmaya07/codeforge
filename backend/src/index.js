dceaabd85a097e1050f021cf9c507a44d76d1987
user.email <cs172668@gmail.com>
2026-04-20 00:03:15 +0530

    Fix cors Error

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const videoRouter = require("./routes/videoCreator");
 const cors = require('cors')
 
 app.use(cors({
-  origin: 'https://codeforge-chinmaya.vercel.app',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 
